41c00e7528840e2cfedb8a475ebf50d0ce0320ed
Patrick Easton SE2016 <patrick.easton.2017@uni.strath.ac.uk>
2019-02-23 21:21:33 +0000

    changed links and how ranking is displayed on ranking.html, changed names of articles

--- ranking.php
+++ ranking.php
@@ -7,15 +7,20 @@ if($_POST['rank1']) {
     $rank4 = $_POST['rank4'];
     $rank5 = $_POST['rank5'];
 
-    $score = array("Article 1"=>$rank1, "Article 2"=>$rank2, "Article 3"=>$rank3,  "Article 4"=>$rank4, "Article 5"=>$rank5);
+    $score = array("Lorem ipsum dolor sit amet, consectetur adipiscing elit"=>$rank1,
+        "Praesent bibendum nec velit a fringilla. Nulla facilisi"=>$rank2,
+        "Vestibulum posuere orci ullamcorper nisi porta, sit amet tempus nibh porta"=>$rank3,
+        "Maecenas venenatis lorem ut erat dictum, sed varius est porta"=>$rank4,
+        "Nulla nibh erat, pharetra at ultricies nec, tincidunt luctus arcu"=>$rank5);
+
     arsort($score);
 
+    $number = 1;
+
     foreach($score as $name => $rank) {
-        echo $name . " has the ranking of " . $rank;
-        echo "<br>";
+        echo $number . ". " . $name;
+        $number++;
+        echo "<br><br>";
     }
-}
-else {
-    echo 'nothing';
 }
 ?>

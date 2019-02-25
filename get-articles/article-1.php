a94f5b4fced4d5b4e634e28b9303ba89ccd8ab2e
Patrick Easton SE2016 <patrick.easton.2017@uni.strath.ac.uk>
2019-02-25 12:36:38 +0000

    getting JSON files from github using php get request, still using AJAX to get contents

---
+++ get-articles/article-1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: patri
+ * Date: 25/02/2019
+ * Time: 11:50
+ */
+
+$JSON = file_get_contents("https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-1.json");
+echo $JSON;
+?>
---
+++ get-articles/article-4.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: patri
+ * Date: 25/02/2019
+ * Time: 11:56
+ */
+$JSON = file_get_contents("https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-4.json");
+echo $JSON;
+?>
